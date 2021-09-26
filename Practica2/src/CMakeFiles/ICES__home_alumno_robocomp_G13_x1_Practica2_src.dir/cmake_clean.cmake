dc1a12a8e9e7c5c75e2ee68ef4219478569b2b36
david00dre <david00dre@gmail.com>
2021-09-26 23:32:19 +0200

    Pasar practica 2 de zip a directorio

---
+++ Practica2/src/CMakeFiles/ICES__home_alumno_robocomp_G13_x1_Practica2_src.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ICES__home_alumno_robocomp_G13_x1_Practica2_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
